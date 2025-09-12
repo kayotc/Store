3a4e571ba0d5c330a8e2565ca9e66fe1688b4787
Kayotc <kayosistem@gmail.com>
2025-09-12 19:06:05 -0300

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+const btnCar = document.querySelector('.btn-car');
+const cleanCar = document.querySelector('.clean-car');
+
+let count = 0;
+
+btnCar.addEventListener('click', ()=>{
+    const car = document.querySelector('#car-count');
+    count++;
+    car.innerHTML = count;
+});
+
+
+cleanCar.addEventListener('click', ()=>{
+    const car = document.querySelector('#car-count');
+    count = 0;
+    car.innerHTML = "";
+});
+
